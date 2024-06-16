1bbb59effe80d9b35344b323a36c99816bfac9f2
gabrielliosc <gabrielli.osc@gmail.com>
2024-06-16 04:18:58 -0300

    style: Create global and theme style files

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,17 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle`
+
+export default createGlobalStyle`    
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Roboto Slab', serif;
+        font-family: 'Roboto', serif;
     }
     
     body{
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
         color: ${({ theme }) => theme.COLORS.WHITE};
         -webkit-font-smoothing: antialiased;
+        font-family: 'Roboto', serif;
     }
 
     body, input, textarea, button {
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,14 +5,14 @@ export default {
         PRIMARY_LIGHT: "#FFA170",
         PRIMARY_LIGHTER: "#FFC8A8",
 
-        SECONDARY_MAIN: "#E63330",
-        SECONDARY_DARK: "#9E1816",
-        SECONDARY_LIGHT: "#2FFA3A1",
-        SECONDARY_LIGHTER: "#FFC9C8",
+        SUPORT_INFORMATION: "#B09A8A",
+        SUPORT_TAG: "#6A49CA",
+        SUPORT_BACKGROUND: "#FEFEFE",
+        SUPORT_BACKGROUND_200: "#F6F6FA",
 
         NEUTRAL: "#1C1E21",
         NEUTRAL_900: "#313338",
-        NEUTRAL_700: "606770",
+        NEUTRAL_700: "#606770",
         NEUTRAL_600: "#8D949E",
         NEUTRAL_500: "BEC3C9", 
         NEUTRAL_400: "#CCD0D4",
@@ -20,6 +20,12 @@ export default {
         NEUTRAL_200: "#EBEDF0",
         NEUTRAL_100: "#F2F3F5",
         NEUTRAL_50: "#F5F6F7",
-        NEUTRAL_WHITE: "#FFFFFF"
+        NEUTRAL_WHITE: "#FFFFFF",
+
+        FEEDBACK_WARNING: "#EACC31",
+        FEEDBACK_DANGER: "#CC1F1F",
+        FEEDBACK_SUCCESS_MAIN: "#6EBF2F",
+        FEEDBACK_SUCCESS_900: "#19A64B",
+        FEEDBACK_SUCCESS_800: "#1FC75B"
     }
 }
