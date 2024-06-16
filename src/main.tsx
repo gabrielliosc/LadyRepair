f64db1ba86f3d5b9f17f98e61c28aab8a2b7fed6
gabrielliosc <gabrielli.osc@gmail.com>
2024-06-16 04:21:44 -0300

    feat: add theme provider, globalstyles and swiper to main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme.ts'
+
+import { register } from 'swiper/element'
+
+register()
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>
+</React.StrictMode>
 )
