1c3c8e042709337b7b0d1feb5c94974270946d88
gabrielliosc <gabrielli.osc@gmail.com>
2024-06-16 04:17:41 -0300

    feat: Add Suggestion component

---
+++ src/components/Suggestion/index.tsx
@@ -0,0 +1,35 @@
+import { Card } from "../Card"
+
+import mockupImg from '../../assets/mockup-1.png'
+import { SuggestionContainer } from "./style"
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+
+export function Suggestion() {
+
+    const contractors = [
+        { id: '1', image: mockupImg, name: 'Maria da Silva Joaquina', price: 100, location: 'Vila Mariana - São Paulo / SP', rating: 4, verified: true, category: 'Pintora' }, 
+        { id: '2', image: 'https://www.google.com', name: 'Maria da Silva Joaquina', price: 100, location: 'Flamengo - Rio de Janeiro / RJ', rating: 3, verified: false, category: 'Eletricista' }, 
+        { id: '3', image: 'https://www.google.com', name: 'Maria da Silva Joaquina', price: 100, location: 'Vila Mariana - São Paulo / SP', rating: 2, verified: false, category: 'Encanadora' }, 
+        { id: '4', image: 'https://www.google.com', name: 'Maria da Silva Joaquina', price: 100, location: 'Vila Mariana - São Paulo / SP', rating: 1, verified: true, category: 'Mêcanica' }, 
+        { id: '5', image: 'https://www.google.com', name: 'Maria da Silva Joaquina', price: 100, location: 'Flamengo - Rio de Janeiro / RJ', rating: 4, verified: true, category: 'Pintora' }
+    ]
+
+    return (
+        <SuggestionContainer>
+            <span>Recomendados</span>
+            <h2>Profissionais mais recomendados</h2>
+            <Swiper 
+                spaceBetween={30}
+                slidesPerView={'auto'}
+                simulateTouch={true}
+            >
+                {
+                contractors.map(contractor => (
+                    <SwiperSlide key={contractor.id} ><Card {...contractor} /></SwiperSlide>))
+                }
+            </Swiper>
+        </SuggestionContainer>
+    )
+}
---
+++ src/components/Suggestion/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const SuggestionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    margin: 0 auto;
+    
+    .swiper {
+        width: 100%;
+        height: 100%;
+    }
+    .swiper-slide {
+        width: 16.5rem;
+    }
+`
