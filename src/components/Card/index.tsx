db02b0ef5de571b3e9924220000be76c0257346a
gabrielliosc <gabrielli.osc@gmail.com>
2024-06-16 23:37:53 -0300

    style: Update and finish card style

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,9 @@
-import { CardContainer, CardTitle } from "./style"
+import { CardContainer, CardTitle, CardContent, CardLocation, CardVerified, CardCategory, CardStars, CardPrice, CardFooter, CardTag } from "./style"
+// import { EmptyStar, Star } from "../Icons"
+import { Bookmark } from '@styled-icons/bootstrap/Bookmark'
+import { Location } from '@styled-icons/fluentui-system-regular/Location'
+import { Star } from'@styled-icons/bootstrap/Star'
+import { StarFill } from'@styled-icons/bootstrap/StarFill'
 
 interface CardProps {
     id: string,
@@ -12,28 +17,40 @@ interface CardProps {
 }
 
 export function Card(contractor: CardProps) {
-    // const stars = [
-    //     contractor.rating >= 1? <Star/>: <EmptyStar />,
-    //     contractor.rating >= 2? <Star/>: <EmptyStar />,
-    //     contractor.rating >= 3? <Star/>: <EmptyStar />,
-    //     contractor.rating >= 4? <Star/>: <EmptyStar />,
-    //     contractor.rating == 5? <Star/>: <EmptyStar />]
+    const stars = [
+        contractor.rating >= 1? <StarFill size={14}/>: <Star size={14} />,
+        contractor.rating >= 2? <StarFill size={14}/>: <Star size={14} />,
+        contractor.rating >= 3? <StarFill size={14}/>: <Star size={14} />,
+        contractor.rating >= 4? <StarFill size={14}/>: <Star size={14} />,
+        contractor.rating == 5? <StarFill size={14}/>: <Star size={14} />]
+
     return (
         <CardContainer>
+            <CardTag>Destaque</CardTag>
             <img src={contractor.image} alt="" />
+            <CardContent>
+                <CardTitle>
+                    <span>{contractor.name}</span>
+                    <span><Bookmark size={20}/></span>
+                </CardTitle>
+                
+                <CardCategory>
+                    {contractor.category}
+                </CardCategory>
 
-            <CardTitle>
-                <span>{contractor.name}</span>
-                <span>icone</span>
-            </CardTitle>
-            
-            <span>{contractor.category}</span>
-            <span>{contractor.location}</span>
-            {contractor.verified && <span>Perfil verificado</span>}
-            <div>
-                {/* <span>{stars}</span> */}
-                <span>{contractor.price}</span>
-            </div>
+                <CardLocation>
+                    <Location size={12}/>
+                    {contractor.location}
+                </CardLocation>
+
+                {contractor.verified && 
+                <CardVerified>Perfil verificado</CardVerified>}
+
+                <CardFooter>
+                    <CardStars>{stars}</CardStars>
+                    <CardPrice>R${contractor.price}<span>/hora</span></CardPrice>
+                </CardFooter>
+            </CardContent>
         </CardContainer>
     )
 }
--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -4,25 +4,100 @@ export const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    color: #313338;
+    font-family: 'Roboto', sans-serif;
+    color: ${({ theme }) => theme.COLORS.NEUTRAL_900};
     border-radius: 0.5rem;
     border: 1px solid #DBDDE1;
     height: 25rem;
-    width: 16.5rem;
-    
+    width: 18.625rem;
+    margin-top: 4rem;
+    position: relative;
+
     img {
         height: 12.5rem;
-        width: 16.5rem;
+        width: 18.625rem;
         object-fit: cover;
         border-top-right-radius: 0.5rem;
         border-top-left-radius: 0.5rem;
     }
 `;
 
+export const CardContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 1rem;
+    gap: 1rem;
+    width: 100%;
+`;
+
+export const CardTag = styled.span`
+    font-size: 0.75rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.COLORS.NEUTRAL_WHITE};
+    background-color: ${({ theme }) => theme.COLORS.SUPORT_TAG};
+    border-radius: 0.25rem;
+    padding: 0.0625rem 0.5rem;
+    position: absolute;
+    top: 0.75rem;
+    left: 0.75rem;
+`
 export const CardTitle = styled.h3`
     font-size: 1rem;
-    color: #313338;
+    color: ${({ theme }) => theme.COLORS.NEUTRAL_900};
+    font-weight: 500;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    width: 100%;
+`
+export const CardCategory = styled.span`
+    font-size: 0.875rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.COLORS.NEUTRAL_700};
+    margin-top: -0.75rem;
+`
+
+export const CardLocation = styled.div`
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.COLORS.NEUTRAL_700};
+    display: flex;
+    align-items: center;
+    gap: 0.28rem;
+`
+
+export const CardVerified = styled.span`
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.COLORS.PRIMARY_MAIN};
+    border: 1px solid ${({ theme }) => theme.COLORS.PRIMARY_MAIN};
+    border-radius: 0.25rem;
+    padding: 0.125rem 0.75rem;
+    font-size: 0.75rem;
+    font-weight: 400;
+    background-color: ${({ theme }) => theme.COLORS.PRIMARY_MAIN_BACKGROUND};
+`
+export const CardStars = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    color: ${({ theme }) => theme.COLORS.PRIMARY_MAIN};
+`
+export const CardPrice = styled.div`
+    font-size: 1rem;
+    color: ${({ theme }) => theme.COLORS.PRIMARY_MAIN};
+    font-weight: 700;
+
+    span {
+        font-size: 0.75rem;
+        color: ${({ theme }) => theme.COLORS.NEUTRAL_700};
+        font-weight: 400;
+        margin-left: 0.25rem;
+    }
+`
+export const CardFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
 `
