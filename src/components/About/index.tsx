d9c8cf49c7071ba3c89a955056ab14758a60589b
gabrielliosc <gabrielli.osc@gmail.com>
2024-06-23 00:34:54 -0300

    feat: Add search fields on about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,10 +1,18 @@
 import { SectionTitle } from "../SectionTitle";
-import { AboutContainer, StepContainer } from "./style";
+import { Search } from "../Search";
+import { AboutContainer, StepContainer, AboutSearchContainer } from "./style";
 
 export function About(){
     return (
         <AboutContainer>
             <SectionTitle title="Quero contratar um serviço" subtitle="Como funciona?" />
+            
+            <AboutSearchContainer>
+                <h2>Encontre profissionais capacitados e solucione seus problemas.</h2>
+                <p>Não perca tempo e agende agora com as melhores prestadoras de serviços.</p>
+
+                <Search />
+            </AboutSearchContainer>           
 
             <ul>
                 <StepContainer>
--- src/components/About/style.ts
+++ src/components/About/style.ts
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 export const AboutContainer = styled.section`
+    font-family: 'Roboto', sans-serif;
     padding: 0 1rem;
 
     ul {
         display: flex;
         flex-direction: column;
-        gap: 4rem;
+        gap: 1.5rem;
     }
 `
-
 export const StepContainer = styled.li`
     border: 3px solid ${({ theme }) => theme.COLORS.PRIMARY_MAIN};
     border-radius: 0.5rem;
-    padding: 2rem 1.5rem;
+    padding: 2rem 1rem;
     display: flex;
-    justify-content: space-between;
     align-items: center;
+    gap: 1rem;
 
     strong {
         font-size: 2.5rem;
@@ -25,9 +25,30 @@ export const StepContainer = styled.li`
     }
 
     p {
-        width: 13rem;
         font-size: 1.125rem;
         font-weight: 400;
         color: ${({ theme }) => theme.COLORS.NEUTRAL_700};
     }
+`
+export const AboutSearchContainer = styled.div`
+    h2 {
+        text-align: left;
+        font-size: 1.5rem;
+        font-weight: 700;
+        margin-bottom: 1rem;
+        color: ${({ theme }) => theme.COLORS.NEUTRAL_900};
+    }
+
+    p {
+        text-align: left;
+        font-size: 1.125rem;
+        font-weight: 400;
+        color: ${({ theme }) => theme.COLORS.NEUTRAL_700};
+        margin-bottom: 12.5rem;
+    }
+
+    background-color: ${({ theme }) => theme.COLORS.SUPORT_BACKGROUND_200};
+    padding: 2rem 1rem;
+    margin-top: 4rem;
+    margin-bottom: 1.5rem;
 `
